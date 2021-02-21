9f76daba532588c43f6ae543585566333bdaba38
locoBit <alan_herlo@hotmail.com>
2021-02-20 19:44:44 -0600

    Setting project

---
+++ src/main/kotlin/ttl/rest/api/TtlApplication.kt
@@ -0,0 +1,11 @@
+package ttl.rest.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TtlApplication
+
+fun main(args: Array<String>) {
+	runApplication<TtlApplication>(*args)
+}
---
+++ src/test/kotlin/ttl/rest/api/TtlApplicationTests.kt
@@ -0,0 +1,13 @@
+package ttl.rest.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TtlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
