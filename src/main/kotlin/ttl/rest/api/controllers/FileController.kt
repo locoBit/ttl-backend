fef0928f1d1eb062aa086e5639bffed3c8ab1e5a
locoBit <alan_herlo@hotmail.com>
2021-03-06 11:26:24 -0600

    Cropping image

--- src/main/kotlin/ttl/rest/api/controllers/FileController.kt
+++ src/main/kotlin/ttl/rest/api/controllers/FileController.kt
@@ -8,16 +8,40 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
+import javax.imageio.ImageIO
+
+import java.awt.image.BufferedImage
+import java.io.*
+
+
 @RestController
 public class FileController {
 
     @PostMapping("uploadfile")
     fun uploadFile(@RequestParam(name = "file") file: MultipartFile): ResponseEntity<ByteArray> {
+        val initialStream: InputStream = file.inputStream
+        val uploadFileName: String = file.originalFilename.toString()
+        val buffer = ByteArray(initialStream.available())
+        initialStream.read(buffer)
+
+        val targetFile = File("/home/locobit/IdeaProjects/ttl/uploads/$uploadFileName")
+
+        FileOutputStream(targetFile).use { outStream -> outStream.write(buffer) }
+
+        val bufferedImage: BufferedImage = ImageIO.read(targetFile)
+        val finalHeight = bufferedImage.height / 2
+        val finalWidth = bufferedImage.width / 2
+
+        val croppedImage: BufferedImage = bufferedImage.getSubimage(0, 0, finalWidth, finalHeight)
+
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        ImageIO.write(croppedImage, "png", byteArrayOutputStream)
+        val croppedImageBytes: ByteArray = byteArrayOutputStream.toByteArray()
 
         return ResponseEntity.ok()
             .contentType(MediaType.parseMediaType(file.contentType.toString()))
-            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.name + "\"")
-            .body(file.bytes)
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"$uploadFileName\"")
+            .body(croppedImageBytes)
     }
 }
 
