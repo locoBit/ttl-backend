533d206996858d93a9740f253e4ddc65d6490d4c
locoBit <alan_herlo@hotmail.com>
2021-03-09 17:44:54 -0600

    Creating image service

---
+++ src/main/kotlin/ttl/rest/api/services/ImageService.kt
@@ -0,0 +1,28 @@
+package ttl.rest.api.services
+
+import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import ttl.rest.api.utils.ImageUtils
+import java.awt.image.BufferedImage
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+import javax.imageio.ImageIO
+
+private const val PIXEL_SIZE : Int = 5;
+
+@Service
+class ImageService(var imageUtils: ImageUtils)
+{
+
+    fun pixelateImage(fileRequest: MultipartFile) : ByteArray
+    {
+        val uploadFileInputStream: InputStream = fileRequest.inputStream
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        val image : BufferedImage = ImageIO.read(uploadFileInputStream)
+        val pixelatedImage: BufferedImage? = imageUtils.pixelate(image, PIXEL_SIZE)
+
+        ImageIO.write(pixelatedImage, "png", byteArrayOutputStream)
+
+        return byteArrayOutputStream.toByteArray()
+    }
+}
